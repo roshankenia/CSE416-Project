adb2246016dad14f872a356bd0b13759472dfabe
Roshan Kenia <62453990+roshankenia@users.noreply.github.com>
2022-03-25 11:11:55 -0400

    Added Font

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,7 @@ import { React } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { AuthContextProvider } from "./auth";
 import { GlobalCommunityContextProvider } from "./community";
-import {
-  WelcomeScreen
-} from "./components";
+import { WelcomeScreen } from "./components";
 /*
     This is our application's top-level component.
     
@@ -17,12 +15,22 @@ import {
   
   @author McKilla Gorilla
 */
+import { ThemeProvider, createMuiTheme } from "@mui/material/styles";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: ["Poor Story"].join(","),
+  },
+});
+
 const App = () => {
   return (
     <BrowserRouter>
       <AuthContextProvider>
         <GlobalCommunityContextProvider>
-          <WelcomeScreen />
+          <ThemeProvider theme={theme}>
+            <WelcomeScreen />
+          </ThemeProvider>
         </GlobalCommunityContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
