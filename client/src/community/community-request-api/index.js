66621f419b89974cf2416b007d46fb9f92a2a423
Alan Yi <Alan.yi@stonybrook.edu>
2022-03-29 23:18:14 -0400

    undo base url

--- client/src/community/community-request-api/index.js
+++ client/src/community/community-request-api/index.js
@@ -13,8 +13,7 @@
 import axios from "axios";
 axios.defaults.withCredentials = true;
 const api = axios.create({
-  baseURL: "https://cse-416-jart.herokuapp.com:4000/api",
-  //baseURL: "http://localhost:4000/api",
+  baseURL: "http://localhost:4000/api",
 });
 
 export const createCommunity = (name, members) => {
