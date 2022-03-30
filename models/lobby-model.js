3dc8bf500d5cd8cb6a9b53e72effee5589d3c85b
Roshan Kenia <62453990+roshankenia@users.noreply.github.com>
2022-03-30 17:41:11 -0400

    Fixed Formatting

--- models/lobby-model.js
+++ models/lobby-model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const User = require("user-model");
+const User = require("./user-model");
 
 const LobbySchema = new Schema(
     {   
