75e476cb168a2e7a299887756646fe0c4bc709bc
Alan Yi <Alan.yi@stonybrook.edu>
2022-03-31 21:56:20 -0400

    changepassword

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -6,6 +6,6 @@ router.post('/register', AuthController.registerUser)
 router.post('/login', AuthController.loginUser)
 router.get('/logout', AuthController.logoutUser)
 router.get('/loggedIn', AuthController.getLoggedIn)
-router.get('/changePassword',AuthController.changePassword)
+router.post('/changePassword', AuthController.changePassword)
 
 module.exports = router
