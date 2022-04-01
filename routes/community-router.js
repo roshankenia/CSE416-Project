5327431946f88e0564fcb84e6928c1af1a15fb83
Roshan Kenia <62453990+roshankenia@users.noreply.github.com>
2022-03-31 22:09:08 -0400

    Update community-router.js

--- routes/community-router.js
+++ routes/community-router.js
@@ -3,11 +3,59 @@ const router = express.Router();
 const auth = require("../auth");
 const CommunityController = require("../controllers/community-controller");
 
+//handles creating a new commmunity in database request
 router.post("/community", auth.verify, CommunityController.createCommunity);
-router.get("/communities", auth.verify, CommunityController.getCommunityList);
+
+//handles a get community request
+router.get("/community/:id", auth.verify, CommunityController.getCommunityById);
+
+//handles an update community request
 router.put("/community/:id", auth.verify, CommunityController.updateCommunityById);
 
+//handles creating a new story in database request
+router.post("/story", auth.verify, CommunityController.createStory);
+
+//handles a get story request
+router.get("/story/:id", auth.verify, CommunityController.getStoryById);
+
+//handles a delete story request
+router.delete("/story/:id", auth.verify, CommunityController.deleteCommunity);
+
+//handles creating a new comic in database request
+router.post("/comic", auth.verify, CommunityController.createComic);
+
+//handles a get comic request
+router.get("/comic/:id", auth.verify, CommunityController.getComicById);
+
+//handles a delete comic request
+router.delete("/comic/:id", auth.verify, CommunityController.deleteComic);
+
+//handles creating a new post in database request
+router.post("/post", auth.verify, CommunityController.createPost);
+
+//handles a get post request
+router.get("/post/:id", auth.verify, CommunityController.getPostById);
+
+//handles an update post request
+router.put("/post/:id", auth.verify, CommunityController.updatePost);
+
+//handles creating a new comment in database request
+router.post("/comment", auth.verify, CommunityController.createComment);
+
+//handles a get comment request
+router.get("/comment/:id", auth.verify, CommunityController.getCommentById);
+
+//handles an update comment request
+router.put("/comment/:id", auth.verify, CommunityController.updateComment);
+
+//handles a search community request
+router.get("/community/", auth.verify, CommunityController.searchCommunity);
+
 //for testing purpose only
-router.post("/deletecommunity", auth.verify, CommunityController.deleteCommunity);
+router.post(
+  "/deletecommunity",
+  auth.verify,
+  CommunityController.deleteCommunity
+);
 
 module.exports = router;
