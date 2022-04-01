0f2126c5255485f85edb1a61a1456124d29bd138
Terran <nikolaterranthe1@gmail.com>
2022-04-01 03:35:48 -0400

    final attempt

--- controllers/community-controller.js
+++ controllers/community-controller.js
@@ -664,7 +664,7 @@ updateComment = async (req, res) => {
 
 searchCommunityByName = async (req, res) => {
   try {
-    const name =  req.query.name;
+    const name =  req.params.name;
     console.log("Finding Community with name: " + JSON.stringify(id));
 
     await Community.find({ communityName : { "$regex": name, "$options": "i" } }, (err, community) => {
