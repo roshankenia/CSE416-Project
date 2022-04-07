5b731da2cfe569f5b58a8ce117394f757bf7f48c
Roshan Kenia <62453990+roshankenia@users.noreply.github.com>
2022-04-07 11:32:32 -0400

    Added modals for change password, delete account, and  feedback

--- client/src/components/AppBanner.js
+++ client/src/components/AppBanner.js
@@ -1,141 +1,177 @@
-import { useContext, useState } from 'react';
-import { Link } from 'react-router-dom'
-import AuthContext from '../auth';
-import { GlobalCommunityContext } from '../community'
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import AuthContext from "../auth";
+import { GlobalCommunityContext } from "../community";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import DeleteAccountModal from "./DeleteAccountModal";
+import FeedbackModal from "./FeedbackModal";
+import ChangePasswordModal from "./ChangePasswordModal";
 
 export default function AppBanner() {
-    const { auth } = useContext(AuthContext);
-    const { community } = useContext(GlobalCommunityContext);
-    const [anchorEl, setAnchorEl] = useState(null);
-    const isMenuOpen = Boolean(anchorEl);
+  const { auth } = useContext(AuthContext);
+  const { community } = useContext(GlobalCommunityContext);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isMenuOpen = Boolean(anchorEl);
 
-    const handleProfileMenuOpen = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
+  const handleOpenDeleteAccount = (event) => {
+    event.preventDefault();
+    community.setDeleteAccount(true);
+  };
 
-    const handleMenuClose = () => {
-        setAnchorEl(null);
-    };
+  const handleOpenChangePassword = (event) => {
+    event.preventDefault();
+    community.setChangePassword(true);
+  };
 
-    const handleLogout = () => {
-        handleMenuClose();
-        // store.resetCurrentList()
-        auth.logoutUser();
-    }
+  const handleOpenFeedback = (event) => {
+    event.preventDefault();
+    community.setFeedback(true);
+  };
 
-    const handleChangePassword = () => {
-    }
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    const menuId = 'primary-search-account-menu';
-    const loggedOutMenu = (
-        <Menu
-            anchorEl={anchorEl}
-            anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-            }}
-            id={menuId}
-            keepMounted
-            transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-            }}
-            open={isMenuOpen}
-            onClose={handleMenuClose}
-        >
-            <MenuItem onClick={handleMenuClose}><Link to='/login/'>Login</Link></MenuItem>
-            <MenuItem onClick={handleMenuClose}><Link to='/register/'>Create New Account</Link></MenuItem>
-        </Menu>
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    handleMenuClose();
+    // store.resetCurrentList()
+    auth.logoutUser();
+  };
+
+  const handleChangePassword = () => {};
+
+  const menuId = "primary-search-account-menu";
+  const loggedOutMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>
+        <Link to="/login/">Login</Link>
+      </MenuItem>
+      <MenuItem onClick={handleMenuClose}>
+        <Link to="/register/">Create New Account</Link>
+      </MenuItem>
+    </Menu>
+  );
+  let username = "";
+  if (auth.loggedIn) {
+    username = auth.user.username;
+  }
+  const loggedInMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "right",
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <Typography> {"Hi " + username} </Typography>
+      <MenuItem onClick={handleLogout}>
+        View Profile(need implementation)
+      </MenuItem>
+      <MenuItem onClick={handleOpenChangePassword}>Change Password</MenuItem>
+      <MenuItem onClick={handleOpenDeleteAccount}>Delete Account</MenuItem>
+      <MenuItem onClick={handleOpenFeedback}>Send Feedback</MenuItem>
+      <MenuItem onClick={handleLogout}>Logout</MenuItem>
+    </Menu>
+  );
+
+  let menu = loggedOutMenu;
+
+  let test = <Box></Box>;
+
+  if (auth.loggedIn) {
+    menu = loggedInMenu;
+    test = (
+      <Typography
+        variant="h4"
+        noWrap
+        component="div"
+        sx={{ display: { xs: "none", sm: "block" } }}
+      >
+        <Link style={{ textDecoration: "none", color: "white" }} to="/test">
+          TEST
+        </Link>
+      </Typography>
     );
-    const loggedInMenu = 
-        <Menu
-            anchorEl={anchorEl}
-            anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right',
-            }}
-            id={menuId}
-            keepMounted
-            transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-            }}
-            open={isMenuOpen}
-            onClose={handleMenuClose}
-        >
-            <Typography>  {'Hi <Insert UserName>'} </Typography>
-            <MenuItem onClick={handleLogout}>View Profile(need implementation)</MenuItem>
-            <MenuItem onClick={handleLogout}>Change Password(need implementation)</MenuItem>
-            <MenuItem onClick={handleLogout}>Delete Account(need implementation)</MenuItem>
-            <MenuItem onClick={handleLogout}>Send Feedback(need implementation)</MenuItem>
-            <MenuItem onClick={handleLogout}>Logout</MenuItem>
-        </Menu>        
+  }
 
-    let menu = loggedOutMenu;
-
-    let test = <Box></Box> 
-
-    if (auth.loggedIn) {
-        menu = loggedInMenu;
-        test = <Typography                        
-                    variant="h4"
-                    noWrap
-                    component="div"
-                    sx={{ display: { xs: 'none', sm: 'block' } }}                        
-                >
-                    <Link style={{ textDecoration: 'none', color: 'white' }} to='/test'>TEST</Link>
-                </Typography>
+  function getAccountMenu(loggedIn) {
+    if (loggedIn) {
+      let initials =
+        auth.user.firstName.substring(0, 1) +
+        auth.user.lastName.substring(0, 1);
+      return <div>{initials}</div>;
+    } else {
+      return <AccountCircle />;
     }
-    
-    function getAccountMenu(loggedIn) {
-        if(loggedIn){
-            let initials = auth.user.firstName.substring(0,1) + auth.user.lastName.substring(0,1)
-            return <div>{initials}</div>
-        }else{
-            return <AccountCircle />
-        }
-    }
+  }
 
-    return (
-        <Box>
-            <AppBar>
-                <Toolbar sx={{flexDirection:'row',justifyContent : 'space-between'}}>
-                    <Typography                        
-                        variant="h4"
-                        noWrap
-                        component="div"
-                        sx={{ display: { xs: 'none', sm: 'block' } }}                        
-                    >
-                        <Link style={{ textDecoration: 'none', color: 'white' }} to='/'>JART</Link>
-                    </Typography>
-                    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-                        <IconButton
-                            size="large"
-                            edge="end"
-                            aria-label="account of current user"
-                            aria-controls={menuId}
-                            aria-haspopup="true"
-                            onClick={handleProfileMenuOpen}
-                            color="inherit"
-                        >
-                            { getAccountMenu(auth.loggedIn) }
-                        </IconButton>
-                    </Box>
-                    {test}
-                </Toolbar>
-            </AppBar>
-            {
-                menu
-            }
-        </Box>
-    );
+  return (
+    <Box>
+      <AppBar>
+        <Toolbar sx={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <Typography
+            variant="h4"
+            noWrap
+            component="div"
+            sx={{ display: { xs: "none", sm: "block" } }}
+          >
+            <Link style={{ textDecoration: "none", color: "white" }} to="/">
+              JART
+            </Link>
+          </Typography>
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              {getAccountMenu(auth.loggedIn)}
+            </IconButton>
+          </Box>
+          {test}
+        </Toolbar>
+      </AppBar>
+      {menu}
+      <DeleteAccountModal />
+      <FeedbackModal />
+      <ChangePasswordModal />
+    </Box>
+  );
 }
--- client/src/components/ChangePasswordModal.js
+++ client/src/components/ChangePasswordModal.js
@@ -1,6 +1,111 @@
 import React, { useContext, useEffect, useState } from "react";
 import { GlobalCommunityContext } from "../community";
+import AuthContext from "../auth";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
 
-export default function ChangePasswordModal(){
-    
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  textAlign: "center",
+  p: 4,
+};
+export default function ChangePasswordModal() {
+  const { auth } = useContext(AuthContext);
+  const { community } = useContext(GlobalCommunityContext);
+
+  function handleChangePassword(event) {
+    // auth.deleteAccount();
+  }
+  function handleClose(event) {
+    community.setChangePassword(false);
+  }
+  return (
+    <Modal
+      open={community.changePasswordModal}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <TextField
+          name="oldPass"
+          fullWidth
+          id="oldPass"
+          label="Old Password:"
+          autoFocus
+          variant="standard"
+          InputProps={{
+            disableUnderline: true,
+            style: {
+              fontSize: 30,
+              paddingLeft: 20,
+              paddingBottom: 10,
+            },
+          }}
+          InputLabelProps={{
+            style: { fontSize: 30, paddingLeft: 20 },
+            shrink: true,
+          }}
+        />
+        <TextField
+          name="newPass"
+          fullWidth
+          id="newPass"
+          label="New Password:"
+          variant="standard"
+          InputProps={{
+            disableUnderline: true,
+            style: {
+              fontSize: 30,
+              paddingLeft: 20,
+              paddingBottom: 10,
+            },
+          }}
+          InputLabelProps={{
+            style: { fontSize: 30, paddingLeft: 20 },
+            shrink: true,
+          }}
+        />
+        <TextField
+          name="confirmNewPass"
+          fullWidth
+          id="confirmNewPass"
+          label="Confirm New Password:"
+          variant="standard"
+          InputProps={{
+            disableUnderline: true,
+            style: {
+              fontSize: 30,
+              paddingLeft: 20,
+              paddingBottom: 10,
+            },
+          }}
+          InputLabelProps={{
+            style: { fontSize: 30, paddingLeft: 20 },
+            shrink: true,
+          }}
+        />
+        <Button
+          variant="contained"
+          onClick={handleChangePassword}
+          sx={{ m: 1 }}
+        >
+          Confirm
+        </Button>
+        <Button variant="outlined" onClick={handleClose} sx={{ m: 1 }}>
+          Cancel
+        </Button>
+      </Box>
+    </Modal>
+  );
 }
