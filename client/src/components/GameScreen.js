d6ecff87c1e6829a260bd7217fb21aa7aff30cc6
jeffrey-hu-53 <jeffrey.hu@stonybrook.edu>
2022-04-07 00:50:56 -0400

    Update GameScreen.js
    - added background

--- client/src/components/GameScreen.js
+++ client/src/components/GameScreen.js
@@ -1,7 +1,20 @@
 import { Grid, Typography } from "@mui/material";
 import React, { useContext, useEffect, useState } from "react";
 import { GlobalCommunityContext } from "../community";
+import { GameContext } from "../game";
+import { Typography, Box, Grid, Button, List, ListItem, TextField } from "@mui/material";
 
 export default function GameScreen(){
-    return <Grid style={{fontSize:'420px'}}>Not implemented</Grid>
+    // return <Grid style={{fontSize:'420px'}}>Not implemented</Grid>
+    return <Grid
+            container
+            spacing={2}
+            justifyContent="center"
+            alignItems="center"
+            style={{
+              width: "100vw",
+              height: "100vh",
+              backgroundImage: "url('https://i.imgur.com/FQ01edj.jpg')",
+            }}
+          ></Grid>
 }
