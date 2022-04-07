d7663de9049b410f03238c9d3ed934a35890a7c0
Roshan Kenia <62453990+roshankenia@users.noreply.github.com>
2022-04-06 21:28:11 -0400

    Comments and Replies

--- client/src/components/CommentCard.js
+++ client/src/components/CommentCard.js
@@ -1,22 +1,277 @@
-import { useContext, useState } from 'react'
+import { useContext, useState } from "react";
 import { GlobalCommunityContext } from "../community";
-import ListItem from '@mui/material/ListItem';
-import IconButton from '@mui/material/IconButton';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
-import { Grid, Stack, Typography } from '@mui/material';
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-import ThumbDownIcon from '@mui/icons-material/ThumbDown';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ExpandLessIcon from '@mui/icons-material/ExpandLess';
-import { Collapse } from '@mui/material';
-import { Box } from '@mui/system';
-import { List } from '@mui/material';
-import { Link } from 'react-router-dom';
+import ListItem from "@mui/material/ListItem";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Grid, Stack, Typography } from "@mui/material";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ThumbDownIcon from "@mui/icons-material/ThumbDown";
+import CommentIcon from "@mui/icons-material/Comment";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import { Collapse } from "@mui/material";
+import { Box } from "@mui/system";
+import { List } from "@mui/material";
+import { Link } from "react-router-dom";
 
-export default function CommentCard(props){
-    const { communtiy } = useContext(GlobalCommunityContext);
-    const { Obj } = props;
-    return ('hi');
-    
+export default function CommentCard(props) {
+  const { community } = useContext(GlobalCommunityContext);
+  const { comment } = props;
+
+  //Keeps track if post is expanded or not
+  const [expanded, setExpanded] = useState(false);
+
+  function handleExpand(event) {
+    event.stopPropagation();
+    let ex = !expanded;
+    setExpanded(ex);
+    // if (ex) {
+    //   store.updateViews(top5List);
+    // }
+  }
+
+  const replies = comment.reply;
+  const replyFeed = (
+    <List>
+      {replies.map((reply) => (
+        <ListItem key={reply}>
+          <Box
+            variant="contained"
+            color="success"
+            size="large"
+            style={{
+              fontWeight: 600,
+              border: "3px solid",
+              borderColor: "black",
+              backgroundColor: "#c3dbd0",
+              color: "black",
+              fontSize: "48px",
+              borderRadius: 20,
+            }}
+            sx={{ width: "100%" }}
+          >
+            <Grid container alignItems="center">
+              <Grid item xs={2}>
+                <Typography
+                  display="inline"
+                  style={{
+                    fontSize: "22px",
+                    paddingLeft: 10,
+                  }}
+                >
+                  {reply.likes.length}
+                </Typography>
+                <IconButton color="primary">
+                  <ThumbUpIcon
+                    sx={{
+                      width: 20,
+                      height: 20,
+                    }}
+                  />
+                </IconButton>
+                <Typography
+                  display="inline"
+                  style={{
+                    fontSize: "22px",
+                  }}
+                >
+                  {reply.dislikes.length}
+                </Typography>
+                <IconButton color="primary">
+                  <ThumbDownIcon
+                    sx={{
+                      width: 20,
+                      height: 20,
+                    }}
+                  />
+                </IconButton>
+              </Grid>
+              <Grid item xs={10}>
+                <Typography
+                  style={{
+                    fontSize: "22px",
+                  }}
+                >
+                  {reply.username + ": " + reply.reply}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Box>
+        </ListItem>
+      ))}
+    </List>
+  );
+  if (expanded) {
+    return (
+      <Box
+        variant="contained"
+        color="success"
+        size="large"
+        style={{
+          fontWeight: 600,
+          border: "3px solid",
+          borderColor: "black",
+          backgroundColor: "#dfe4ed",
+          color: "black",
+          fontSize: "48px",
+          borderRadius: 20,
+        }}
+        sx={{ width: "100%" }}
+      >
+        <Grid container alignItems="center">
+          <Grid item xs={2.5}>
+            <Typography
+              display="inline"
+              style={{
+                fontSize: "22px",
+                paddingLeft: 10,
+              }}
+            >
+              {comment.likes.length}
+            </Typography>
+            <IconButton color="primary">
+              <ThumbUpIcon
+                sx={{
+                  width: 20,
+                  height: 20,
+                }}
+              />
+            </IconButton>
+            <Typography
+              display="inline"
+              style={{
+                fontSize: "22px",
+              }}
+            >
+              {comment.dislikes.length}
+            </Typography>
+            <IconButton color="primary">
+              <ThumbDownIcon
+                sx={{
+                  width: 20,
+                  height: 20,
+                }}
+              />
+            </IconButton>
+            <Typography
+              display="inline"
+              style={{
+                fontSize: "22px",
+              }}
+            >
+              {comment.reply.length}
+            </Typography>
+            <IconButton color="primary">
+              <CommentIcon
+                onClick={(event) => {
+                  handleExpand(event);
+                }}
+                sx={{
+                  width: 20,
+                  height: 20,
+                }}
+              />
+            </IconButton>
+          </Grid>
+          <Grid item xs={9.5}>
+            <Typography
+              style={{
+                fontSize: "26px",
+              }}
+            >
+              {comment.username + ": " + comment.comment}
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            {replyFeed}
+          </Grid>
+        </Grid>
+      </Box>
+    );
+  } else {
+    return (
+      <Box
+        variant="contained"
+        color="success"
+        size="large"
+        style={{
+          fontWeight: 600,
+          border: "3px solid",
+          borderColor: "black",
+          backgroundColor: "#dfe4ed",
+          color: "black",
+          fontSize: "48px",
+          borderRadius: 20,
+        }}
+        sx={{ width: "100%" }}
+      >
+        <Grid container alignItems="center">
+          <Grid item xs={2.5}>
+            <Typography
+              display="inline"
+              style={{
+                fontSize: "22px",
+                paddingLeft: 10,
+              }}
+            >
+              {comment.likes.length}
+            </Typography>
+            <IconButton color="primary">
+              <ThumbUpIcon
+                sx={{
+                  width: 20,
+                  height: 20,
+                }}
+              />
+            </IconButton>
+            <Typography
+              display="inline"
+              style={{
+                fontSize: "22px",
+              }}
+            >
+              {comment.dislikes.length}
+            </Typography>
+            <IconButton color="primary">
+              <ThumbDownIcon
+                sx={{
+                  width: 20,
+                  height: 20,
+                }}
+              />
+            </IconButton>
+            <Typography
+              display="inline"
+              style={{
+                fontSize: "22px",
+              }}
+            >
+              {comment.reply.length}
+            </Typography>
+            <IconButton color="primary">
+              <CommentIcon
+                onClick={(event) => {
+                  handleExpand(event);
+                }}
+                sx={{
+                  width: 20,
+                  height: 20,
+                }}
+              />
+            </IconButton>
+          </Grid>
+          <Grid item xs={9.5}>
+            <Typography
+              style={{
+                fontSize: "26px",
+              }}
+            >
+              {comment.username + ": " + comment.comment}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+    );
+  }
 }
