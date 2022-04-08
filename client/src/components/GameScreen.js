73889c889e0726466000e14e451ad8d25ed667db
jeffrey-hu-53 <jeffrey.hu@stonybrook.edu>
2022-04-08 16:07:07 -0400

    Update GameScreen.js
    - Comic Game Screen Basic Static Components Completed

--- client/src/components/GameScreen.js
+++ client/src/components/GameScreen.js
@@ -12,6 +12,7 @@ export default function GameScreen(){
     //This is just hardcoded for now
     // THIS DATA SHOULD BE TAKEN FROM THE GAME OBJECT PASSED IN GameContext
     const roomCode = 'imadethiscodeup'
+    const gameMode = "comic"
     const turnNumber = 0
     const players = [
         "u/anon",
@@ -24,10 +25,13 @@ export default function GameScreen(){
     const flexContainer = {
         display: 'flex',
         flexDirection: 'row',
-        padding: 0,
+        padding: 25,
+        align: "center",
+        alignItems: "center"
+        
       };
-
-    return  <Grid
+    if (gameMode === "comic"){
+        return  <Grid
                 container
                 spacing={2}
                 justifyContent="center"
@@ -38,25 +42,228 @@ export default function GameScreen(){
                 backgroundImage: "url('https://i.imgur.com/FQ01edj.jpg')",
                 }}
             >
-                {currentPlayer} + " is currently drawing..."
-                <List style={flexContainer}>
-                    <ListItem
-                    primaryText="foo1"
-                    secondaryText="bar1"/>
-                    <ListItem
-                    primaryText="foo2"
-                    secondaryText="bar2"/>
-                </List>
-                <Box
-                    sx={{
-                        width: 300,
-                        height: 300,
-                        backgroundColor: 'primary.dark',
-                        '&:hover': {
-                        backgroundColor: 'primary.main',
-                        opacity: [0.9, 0.8, 0.7],
-                        },
-                    }}
-                />
+                
+                <Grid item xs="12" align="center">
+                    <Typography fontSize={"32px"}>
+                        {currentPlayer} + " is currently drawing..."
+                    </Typography>
+                
+                    <List style={flexContainer} sx={{justifyContent:"center"}}>
+                        {/* List of current panels drawn goes here */}
+                        <Box
+                            sx={{
+                                width: 150,
+                                height: 150,
+                                margin: 1,
+                                backgroundColor: 'primary.dark',
+                                '&:hover': {
+                                backgroundColor: 'primary.main',
+                                opacity: [0.9, 0.8, 0.7],
+                                },
+                            }}
+                        ></Box>
+                        <Box
+                            sx={{
+                                width: 150,
+                                height: 150,
+                                margin: 1,
+                                backgroundColor: 'primary.dark',
+                                '&:hover': {
+                                backgroundColor: 'primary.main',
+                                opacity: [0.9, 0.8, 0.7],
+                                },
+                            }}
+                        ></Box>
+                        <Box
+                            sx={{
+                                width: 150,
+                                height: 150,
+                                margin: 1,
+                                backgroundColor: 'primary.dark',
+                                '&:hover': {
+                                backgroundColor: 'primary.main',
+                                opacity: [0.9, 0.8, 0.7],
+                                },
+                            }}
+                        ></Box>
+                        <Box
+                            sx={{
+                                width: 150,
+                                height: 150,
+                                margin: 1,
+                                backgroundColor: 'primary.dark',
+                                '&:hover': {
+                                backgroundColor: 'primary.main',
+                                opacity: [0.9, 0.8, 0.7],
+                                },
+                            }}
+                        ></Box>
+                        <Box
+                            sx={{
+                                width: 150,
+                                height: 150,
+                                margin: 1,
+                                backgroundColor: 'primary.dark',
+                                '&:hover': {
+                                backgroundColor: 'primary.main',
+                                opacity: [0.9, 0.8, 0.7],
+                                },
+                            }}
+                        ></Box>
+                        
+                    </List>
+                    
+                    {/* Bottom half of screen */}
+                    <List style={flexContainer} sx={{justifyContent:"center"}}>
+                        {/* Left of Canvas */}
+                        <Grid item xs="3" align="center"> 
+                            <List style={flexContainer}>
+                                <Box
+                                    sx={{
+                                        width: 300,
+                                        height: 600,
+                                        margin: 1,
+                                        backgroundColor: 'primary.dark',
+                                        '&:hover': {
+                                        backgroundColor: 'primary.main',
+                                        opacity: [0.9, 0.8, 0.7],
+                                        },
+                                    }}
+                                ></Box>
+                                <Box
+                                    sx={{
+                                        width: 150,
+                                        height: 600,
+                                        margin: 1,
+                                        backgroundColor: 'primary.dark',
+                                        '&:hover': {
+                                        backgroundColor: 'primary.main',
+                                        opacity: [0.9, 0.8, 0.7],
+                                        },
+                                    }}
+                                ></Box>
+                            </List>
+                        </Grid>
+                        {/* Drawing / Writing Canvas */}
+                        <Grid item xs="6" align="center">
+                            <Box
+                                sx={{
+                                    width: 600,
+                                    height: 600,
+                                    backgroundColor: 'primary.dark',
+                                    '&:hover': {
+                                    backgroundColor: 'primary.main',
+                                    opacity: [0.9, 0.8, 0.7],
+                                    },
+                                }}
+                            ></Box>
+                        </Grid>
+                        {/* Right of Canvas */}
+                        <Grid item xs="3" align="center">
+                            <Box
+                                sx={{
+                                    width: 250,
+                                    height: 50,
+                                    margin: 1,
+                                    backgroundColor: 'primary.dark',
+                                    '&:hover': {
+                                    backgroundColor: 'primary.main',
+                                    opacity: [0.9, 0.8, 0.7],
+                                    },
+                                }}
+                            ></Box>
+                            <Box
+                                sx={{
+                                    width: 250,
+                                    height: 50,
+                                    margin: 1,
+                                    backgroundColor: 'primary.dark',
+                                    '&:hover': {
+                                    backgroundColor: 'primary.main',
+                                    opacity: [0.9, 0.8, 0.7],
+                                    },
+                                }}
+                            ></Box>
+                            <Box
+                                sx={{
+                                    width: 250,
+                                    height: 50,
+                                    margin: 1,
+                                    backgroundColor: 'primary.dark',
+                                    '&:hover': {
+                                    backgroundColor: 'primary.main',
+                                    opacity: [0.9, 0.8, 0.7],
+                                    },
+                                }}
+                            ></Box>
+                            <Box
+                                sx={{
+                                    width: 250,
+                                    height: 50,
+                                    margin: 1,
+                                    backgroundColor: 'primary.dark',
+                                    '&:hover': {
+                                    backgroundColor: 'primary.main',
+                                    opacity: [0.9, 0.8, 0.7],
+                                    },
+                                }}
+                            ></Box>
+                            <Box
+                                sx={{
+                                    width: 250,
+                                    height: 50,
+                                    margin: 1,
+                                    backgroundColor: 'primary.dark',
+                                    '&:hover': {
+                                    backgroundColor: 'primary.main',
+                                    opacity: [0.9, 0.8, 0.7],
+                                    },
+                                }}
+                            ></Box>
+                            <Box
+                                sx={{
+                                    width: 250,
+                                    height: 50,
+                                    margin: 1,
+                                    backgroundColor: 'primary.dark',
+                                    '&:hover': {
+                                    backgroundColor: 'primary.main',
+                                    opacity: [0.9, 0.8, 0.7],
+                                    },
+                                }}
+                            ></Box>
+                        </Grid>
+                    </List>   
+                </Grid> 
             </Grid>
+    } else if (gameMode === "story") {
+        return <Grid
+            container
+            spacing={2}
+            justifyContent="center"
+            alignItems="center"
+            style={{
+            width: "100vw",
+            height: "100vh",
+            backgroundImage: "url('https://i.imgur.com/FQ01edj.jpg')",
+        }}
+        >
+            Story Game Screen Not Implemented
+        </Grid>
+    } else {
+        return <Grid
+            container
+            spacing={2}
+            justifyContent="center"
+            alignItems="center"
+            style={{
+            width: "100vw",
+            height: "100vh",
+            backgroundImage: "url('https://i.imgur.com/FQ01edj.jpg')",
+        }}
+        >
+            Game Mode Invalid
+        </Grid>
+    }
+    
 }
