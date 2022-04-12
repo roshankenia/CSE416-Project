b3dc48583cfc398bf854dffc29e3a01bd6a7a82a
Roshan Kenia <62453990+roshankenia@users.noreply.github.com>
2022-04-12 11:01:00 -0400

    Update auth-controller.js

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -99,6 +99,7 @@ addFriend = async (req, res) => {
 
       //remove request from received user
       for (var i = 0; i < receivedUser.requests.length; i++) {
+        console.log(receivedUser.requests[i]);
         if (receivedUser.requests[i].email == sentUser.email) {
           receivedUser.requests.splice(i, 1);
           break;
