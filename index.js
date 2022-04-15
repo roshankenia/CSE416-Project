886f7c2ade258bb6a21d507704d3a940d3a91a37
Terran <nikolaterranthe1@gmail.com>
2022-04-15 17:45:56 -0400

    retry chat

--- index.js
+++ index.js
@@ -52,7 +52,23 @@ var server = app.listen(PORT, function () {
 
 
 var io = require('socket.io')(server);
-const STATIC_CHANNELS = ['global_notifications', 'global_chat'];
+var STATIC_CHANNELS = [{
+  name: 'Global chat',
+  participants: 0,
+  id: 1,
+  sockets: []
+}, {
+  name: 'Funny',
+  participants: 0,
+  id: 2,
+  sockets: []
+}];
+
+//by pass cors stuff
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+})
 
 io.on('connection', (socket) => { // socket object may be used to send specific messages to the new connected client
   console.log('new client connected');
