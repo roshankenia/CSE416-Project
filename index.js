7b830144d5ad6cbb9bf3af80edebb445d7ac21c7
Roshan Kenia <62453990+roshankenia@users.noreply.github.com>
2022-04-19 22:25:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -128,6 +128,7 @@ io.on("connection", (socket) => {
 
   socket.on("leave-lobby", (username, lobbyID) => {
     console.log(username, "is leaving", lobbyID);
+    socket.leave(lobbyID);
     socket.to(lobbyID).emit("remove-player", username, lobbyID);
   });
 });
