fcdf76eb9aebb94b21cfd58dc68844fb55a755bb
Roshan Kenia <62453990+roshankenia@users.noreply.github.com>
2022-04-24 23:43:42 -0400

    Increase Server Request Size

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "client", "build")));
 
