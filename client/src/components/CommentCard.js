92ac012d3443afcd16bfba68c0761191d59b827c
jeffrey-hu-53 <jeffrey.hu@stonybrook.edu>
2022-05-08 20:09:05 -0400

    Update CommentCard.js
    - removed replies

--- client/src/components/CommentCard.js
+++ client/src/components/CommentCard.js
@@ -1,5 +1,6 @@
 import { useContext, useState } from "react";
 import { GlobalCommunityContext } from "../community";
+import AuthContext from "../auth";
 import ListItem from "@mui/material/ListItem";
 import IconButton from "@mui/material/IconButton";
 import EditIcon from "@mui/icons-material/Edit";
@@ -20,9 +21,12 @@ import TextField from "@mui/material/TextField";
 export default function CommentCard(props) {
   const { community } = useContext(GlobalCommunityContext);
   const { comment } = props;
+  const { auth } = useContext(AuthContext);
+
 
   //Keeps track if post is expanded or not
   const [expanded, setExpanded] = useState(false);
+  // const [reply, setReply] = useState('');
 
   function handleExpand(event) {
     event.stopPropagation();
@@ -33,77 +37,102 @@ export default function CommentCard(props) {
     // }
   }
 
-  const replies = comment.reply;
-  const replyFeed = (
-    <List>
-      {replies.map((reply, index) => (
-        <ListItem key={index}>
-          <Box
-            variant="contained"
-            color="success"
-            size="large"
-            style={{
-              fontWeight: 600,
-              border: "3px solid",
-              borderColor: "black",
-              backgroundColor: "#c3dbd0",
-              color: "black",
-              fontSize: "48px",
-              borderRadius: 20,
-            }}
-            sx={{ width: "100%" }}
-          >
-            <Grid container alignItems="center">
-              <Grid item xs={2}>
-                <Typography
-                  display="inline"
-                  style={{
-                    fontSize: "22px",
-                    paddingLeft: 10,
-                  }}
-                >
-                  {reply.likes.length}
-                </Typography>
-                <IconButton color="primary">
-                  <ThumbUpIcon
-                    sx={{
-                      width: 20,
-                      height: 20,
-                    }}
-                  />
-                </IconButton>
-                <Typography
-                  display="inline"
-                  style={{
-                    fontSize: "22px",
-                  }}
-                >
-                  {reply.dislikes.length}
-                </Typography>
-                <IconButton color="primary">
-                  <ThumbDownIcon
-                    sx={{
-                      width: 20,
-                      height: 20,
-                    }}
-                  />
-                </IconButton>
-              </Grid>
-              <Grid item xs={10}>
-                <Typography
-                  style={{
-                    fontSize: "22px",
-                  }}
-                >
-                  {reply.username + ": " + reply.reply}
-                </Typography>
-              </Grid>
-            </Grid>
-          </Box>
-        </ListItem>
-      ))}
-    </List>
-  );
+  // const enterKeyDown = (event) => {
+  //   if (event.key === "Enter"){
+  //     console.log("Enter was pressed.");
+  //     handleSubmitReply();
+  //     setReply('');
+  //     setExpanded(false);
+  //   }
+  // }
+
+  // function handleSubmitReply(){
+  //   if (reply !== ''){
+  //     console.log("reply submitted:", reply)
+  //     community.updatePost("reply", null, {comment: comment, reply: reply}, auth.user);
+  //   } else {
+  //     console.log("no reply to submit");
+  //   }
+  // }
+
+  // function handleUpdateReply(event) {
+  //   console.log(event.target.value)
+  //   setReply(event.target.value)
+  // }
+
+  // const replies = comment.reply;
+  // console.log(comment)
+  // console.log(replies)
+  // const replyFeed = (
+  //   <List>
+  //     {replies.map((reply, index) => (
+  //       <ListItem key={index}>
+  //         <Box
+  //           variant="contained"
+  //           color="success"
+  //           size="large"
+  //           style={{
+  //             fontWeight: 600,
+  //             border: "3px solid",
+  //             borderColor: "black",
+  //             backgroundColor: "#c3dbd0",
+  //             color: "black",
+  //             fontSize: "48px",
+  //             borderRadius: 20,
+  //           }}
+  //           sx={{ width: "100%" }}
+  //         >
+  //           <Grid container alignItems="center">
+  //             <Grid item xs={2}>
+  //               <Typography
+  //                 display="inline"
+  //                 style={{
+  //                   fontSize: "22px",
+  //                   paddingLeft: 10,
+  //                 }}
+  //               >
+  //                 {reply.likes.length}
+  //               </Typography>
+  //               <IconButton color="primary">
+  //                 <ThumbUpIcon
+  //                   sx={{
+  //                     width: 20,
+  //                     height: 20,
+  //                   }}
+  //                 />
+  //               </IconButton>
+  //               <Typography
+  //                 display="inline"
+  //                 style={{
+  //                   fontSize: "22px",
+  //                 }}
+  //               >
+  //                 {reply.dislikes.length}
+  //               </Typography>
+  //               <IconButton color="primary">
+  //                 <ThumbDownIcon
+  //                   sx={{
+  //                     width: 20,
+  //                     height: 20,
+  //                   }}
+  //                 />
+  //               </IconButton>
+  //             </Grid>
+  //             <Grid item xs={10}>
+  //               <Typography
+  //                 style={{
+  //                   fontSize: "22px",
+  //                 }}
+  //               >
+  //                 {reply.username + ": " + reply.reply}
+  //               </Typography>
+  //             </Grid>
+  //           </Grid>
+  //         </Box>
+  //       </ListItem>
+  //     ))}
+  //   </List>
+  // );
   if (expanded) {
     return (
       <Box
@@ -156,7 +185,7 @@ export default function CommentCard(props) {
                 }}
               />
             </IconButton>
-            <Typography
+            {/* <Typography
               display="inline"
               style={{
                 fontSize: "22px",
@@ -174,7 +203,7 @@ export default function CommentCard(props) {
                   height: 20,
                 }}
               />
-            </IconButton>
+            </IconButton> */}
           </Grid>
           <Grid item xs={9.5}>
             <Typography
@@ -199,7 +228,7 @@ export default function CommentCard(props) {
                 marginLeft: 15,
               }}
             >
-              <Box style={{ width: "96%" }}>
+              {/* <Box style={{ width: "96%" }}>
                 <TextField
                   fullWidth
                   variant="standard"
@@ -217,13 +246,15 @@ export default function CommentCard(props) {
                     style: { fontSize: 24, paddingLeft: 20 },
                     shrink: true,
                   }}
+                  onChange={handleUpdateReply}
+                  onKeyDown={(e) => enterKeyDown(e)}
                 />
-              </Box>
+              </Box> */}
             </Box>
           </Grid>
-          <Grid item xs={12}>
+          {/* <Grid item xs={12}>
             {replyFeed}
-          </Grid>
+          </Grid> */}
         </Grid>
       </Box>
     );
@@ -279,7 +310,7 @@ export default function CommentCard(props) {
                 }}
               />
             </IconButton>
-            <Typography
+            {/* <Typography
               display="inline"
               style={{
                 fontSize: "22px",
@@ -297,7 +328,7 @@ export default function CommentCard(props) {
                   height: 20,
                 }}
               />
-            </IconButton>
+            </IconButton> */}
           </Grid>
           <Grid item xs={9.5}>
             <Typography
