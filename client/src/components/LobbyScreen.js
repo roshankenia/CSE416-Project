987f11a95e6e9fa8f820df4d32e01107959a229b
Roshan Kenia <62453990+roshankenia@users.noreply.github.com>
2022-05-13 22:40:58 -0400

    Update LobbyScreen.js

--- client/src/components/LobbyScreen.js
+++ client/src/components/LobbyScreen.js
@@ -100,7 +100,7 @@ export default function LobbyScreen() {
   //Note, time is the slider value times 3
   const handleStartGame = (event) => {
     console.log(game);
-    game.setTimer(5);
+    game.setTimer(value);
     console.log("game timer(in seconds): " + game.timer);
     game.createNewGame();
   };
