9583069efd7e486f05a5757d68d3aaea89542a30
Terran <nikolaterranthe1@gmail.com>
2022-05-15 23:33:07 -0400

    trying to fix double drop

--- client/src/components/SinglePlayer.js
+++ client/src/components/SinglePlayer.js
@@ -40,7 +40,7 @@ export default function GameScreen() {
 
   const [actions, setActions] = React.useState([]);
   const [storyText, setStoryText] = React.useState("");
-  const charLimit = 9999999;
+  const [charLimit, setCharLimit] = useState(9999999);
 
   let backgroundImages = [
     require("../images/Backgrounds/6-square-cartoon-radial-background-1.jpg"),
@@ -617,6 +617,28 @@ export default function GameScreen() {
     />
   );
 
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    // register event position
+    stageRef.current.setPointersPositions(e);
+    // add image
+
+    actions.push({
+      ...stageRef.current.getPointerPosition(),
+      src: dragUrl.current,
+      key: actions.length + 1,
+      size: strokeWidth,
+    });
+    
+    console.log(actions)
+
+    setActions(actions)
+    //force update
+    setCharLimit(charLimit - 1)
+  }
+
   /* Drawing/Writing Canvas */
   let gameWorkSpace = "";
 
@@ -630,28 +652,7 @@ export default function GameScreen() {
             backgroundColor: "white",
             border: 3,
           }}
-          onDrop={(e) => {
-            e.preventDefault();
-            // register event position
-            stageRef.current.setPointersPositions(e);
-            // add image
-            actions.push({
-              ...stageRef.current.getPointerPosition(),
-              src: dragUrl.current,
-              key: actions.length + 1,
-              size: strokeWidth,
-            });
-            setActions(
-              actions.concat([
-                {
-                  ...stageRef.current.getPointerPosition(),
-                  src: dragUrl.current,
-                  key: actions.length + 1,
-                  size: strokeWidth,
-                },
-              ])
-            );
-          }}
+          onDrop={handleDrop}
           onDragOver={(e) => e.preventDefault()}
         >
           <Stage
